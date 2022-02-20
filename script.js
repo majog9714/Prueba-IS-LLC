233b70c76d618794a1d00f869be6ff81b1360ef8
Maria Jose Guerrero <majog9714@gmail.com>
2022-02-19 22:54:35 -0500

    implementado seleciones en barra de busqueda

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function showSelectRooms() {
+    document.getElementById('item-combo').style.visibility="visible";
+}
+
+function hideSelectRooms(){
+    document.getElementById('item-combo').style.display = "none";
+}
+// onclick="showSelectRooms()" onfocusout="hideSelectRooms()"
