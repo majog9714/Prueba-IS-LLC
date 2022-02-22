b8af9b54f70d9629457fb2d1a253d347fa11e734
Maria Jose Guerrero <majog9714@gmail.com>
2022-02-22 11:04:23 -0500

    Implementación de estilos y animación de slider final

--- script.js
+++ script.js
@@ -6,4 +6,33 @@ function showSelectRooms() {
 function hideSelectRooms(){
     document.getElementById('item-combo').style.display = "none";
 }
+function showMore(id){
+  document.getElementById('lista-enlaces'+id).classList.toggle('active');
+}
+
+const left = document.getElementById('btn-izq');
+const right = document.getElementById('btn-der');
+let i = 1;
+
+right.onclick = () => {
+  if (i >=5){
+    document.getElementById('frase'+i).classList.toggle('active');
+    document.getElementById('frase1').classList.toggle('active');
+    i = 1;
+    return;
+  }
+
+  document.getElementById('frase'+i).classList.toggle('active');
+  document.getElementById('frase'+(i+1)).classList.toggle('active');
+  i++;
+}
+
+left.onclick = () => {
+  if (i==1)
+    return;
+  document.getElementById('frase'+i).classList.toggle('active');
+  document.getElementById('frase'+(i-1)).classList.toggle('active');
+  i--;
+}
+
 // onclick="showSelectRooms()" onfocusout="hideSelectRooms()"
